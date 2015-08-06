dac3f3fa013fcd0ecfa274d7d67ef5c46b26098a
rorpassion <dennischan8782@gmail.com>
2015-08-06 14:50:51 -0400

    Starting on Property

---
+++ app/controllers/PropertyController.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+* Manage Property for API endpoints
+*/
+class PropertyController extends BaseController {
+    
+    /**
+     * Create a new property
+     */
+    public function create($user_id)
+    {   
+        $validator = Validator::make($data = Input::all(), Property::$rules);
+        
+        if ($validator->fails())
+        {
+            return Response::json($validator->messages(), 400);
+        }
+        
+        try 
+        {
+            $property = Property::create($data);
+        }
+        catch (Exception $e)
+        {
+            return Response::json("failure", 400);   
+        }
+        return Response::json($property->toArray(), 200);
+    }
+    
+    /**
+     * Retrieve properties by user
+     */
+    public function index($user_id)
+    {   
+        $properties = Property::where('user_id', $user_id)->get();
+        return Response::json($properties->toArray(), 200);
+    }
+    
+    
+    /**
+    * Retrieve client details by id
+    */
+    public function show($user_id, $id)
+    {
+        $property = Property::find($id);
+        
+        if (!$property)
+        {
+            return Response::json('Property does not exist', 400);
+        }
+        
+        return Response::json($property->toArray(), 200);
+    }
+    
+    /**
+    * Update client details
+    */
+    public function update($user_id, $id)
+    {
+        $validator = Validator::make($data = Input::all(), Property::$rules);
+
+        if ($validator->fails())
+        {
+            return Response::json($validator->messages(), 400);
+        }
+        
+        try
+        {
+            $property = Property::find($id)->update(Input::all());
+        }
+        catch (Exception $e)
+        {
+            return Response::json("failure", 400);
+        }
+        
+        return Response::json($property->toArray(), 200);
+    }
+}  
+?>
---
+++ app/models/Property.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Auth\UserTrait;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableInterface;
+
+class Property extends Eloquent implements UserInterface, RemindableInterface {
+
+    use UserTrait, RemindableTrait;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'properties';
+    protected $fillable = array(
+        'user_id', 'client_id', 'address', 'type', 'phone', 'total_units', 'reserve_amount', 'fee_type', 'fee_amount');
+    
+    /**
+    * Validation Rule
+    */
+    public static $rules = array(
+        'user_id'   => 'required',
+        'client_id' => 'required',
+        'type'      => 'required'
+    );
+
+  
+    /**
+     * One Client belongs to one User
+     */
+    public function client()
+    {
+        return $this->belongsTo("Client");
+    }
+    
+    /**
+    * One Property can have many units
+    */
+    public function units()
+    {
+        return $this->hasMany("Unit");
+    }
+}
